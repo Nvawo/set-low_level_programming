49e70df59e1b57f9886f6dcd328123fef630602b
Nvawo Alamar Nvawo <nyawoalamar5@gmail.com>
2026-06-01 15:27:30 +0300

    Restore lists.h with prototypes

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -3,12 +3,6 @@
 
 #include <stddef.h>
 
-/**
- * struct dlistint_s - doubly linked list node
- * @n: integer
- * @prev: pointer to previous node
- * @next: pointer to next node
- */
 typedef struct dlistint_s
 {
     int n;
@@ -16,8 +10,8 @@ typedef struct dlistint_s
     struct dlistint_s *next;
 } dlistint_t;
 
-/* function prototypes */
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 
 #endif
