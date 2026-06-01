2a776fed02da805297497bd93ba7a736a9535e6f
Nvawo Alamar Nvawo <nyawoalamar5@gmail.com>
2026-06-01 10:28:20 +0300

    Fix insert_nodeint_at_index edge cases

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts a new node at index
- * @head: pointer to head of list
- * @idx: index to insert at
- * @n: value for new node
+ * insert_nodeint_at_index - inserts node at index
+ * @head: pointer to head
+ * @idx: index to insert
+ * @n: value
  *
- * Return: pointer to new node, or NULL on failure
+ * Return: pointer to new node or NULL
  */
 listint_t *insert_nodeint_at_index(listint_t **head,
                                     unsigned int idx, int n)
@@ -18,16 +18,16 @@ listint_t *insert_nodeint_at_index(listint_t **head,
 	if (head == NULL)
 		return (NULL);
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->n = n;
-
 	if (idx == 0)
 	{
+		new_node = malloc(sizeof(listint_t));
+		if (new_node == NULL)
+			return (NULL);
+
+		new_node->n = n;
 		new_node->next = *head;
 		*head = new_node;
+
 		return (new_node);
 	}
 
@@ -37,11 +37,13 @@ listint_t *insert_nodeint_at_index(listint_t **head,
 		temp = temp->next;
 
 	if (temp == NULL)
-	{
-		free(new_node);
 		return (NULL);
-	}
 
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
 	new_node->next = temp->next;
 	temp->next = new_node;
 
