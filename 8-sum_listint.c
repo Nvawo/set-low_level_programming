1f6dad52765cd894a5b96284b6fd16868fe9898b
Nvawo Alamar Nvawo <nyawoalamar5@gmail.com>
2026-06-01 08:53:18 +0300

    Add sum_listint function

--- 8-sum_listint.c
+++ 8-sum_listint.c
@@ -1,20 +1,21 @@
 #include "lists.h"
 
 /**
- * sum_listint - returns sum of all n in list
- * @head: pointer to list
+ * sum_listint - returns the sum of all the data (n)
+ * of a listint_t linked list
+ * @head: pointer to the head of the list
  *
- * Return: sum of nodes or 0
+ * Return: sum of all data, or 0 if list is empty
  */
 int sum_listint(listint_t *head)
 {
-    int sum = 0;
+	int sum = 0;
 
-    while (head)
-    {
-        sum += head->n;
-        head = head->next;
-    }
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
 
-    return (sum);
+	return (sum);
 }
