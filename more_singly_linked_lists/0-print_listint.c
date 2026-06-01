c85eec42b2269107a5b04d77587f758890708d95
Nvawo Alamar Nvawo <nyawoalamar5@gmail.com>
2026-06-01 10:22:55 +0300

    Restore required dependency files for linked list tasks

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <stdio.h>
+
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+
+	return (i);
+}
