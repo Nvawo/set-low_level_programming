7bc0e4ad4755232ae17b256c0dc35c9496e77398
Nvawo Alamar Nvawo <nyawoalamar5@gmail.com>
2026-06-01 10:16:13 +0300

    Fix insert_nodeint_at_index ALX compliance

---
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,49 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts a new node at index
+ * @head: pointer to head of list
+ * @idx: index to insert at
+ * @n: value for new node
+ *
+ * Return: pointer to new node, or NULL on failure
+ */
+listint_t *insert_nodeint_at_index(listint_t **head,
+                                    unsigned int idx, int n)
+{
+	listint_t *new_node;
+	listint_t *temp;
+	unsigned int i;
+
+	if (head == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+
+	temp = *head;
+
+	for (i = 0; temp != NULL && i < idx - 1; i++)
+		temp = temp->next;
+
+	if (temp == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->next = temp->next;
+	temp->next = new_node;
+
+	return (new_node);
+}
