15c8059f9648703de724eabfcb6f89d17b92d7d4
Nvawo Alamar Nvawo <nyawoalamar5@gmail.com>
2026-06-01 09:35:59 +0300

    Clean restart: sum_listint implementation

--- README.md
+++
@@ -1,3 +0,0 @@
-More Singly Linked Lists
-
-Task 0: Print all elements of a listint_t list.
--- lists.h
+++ lists.h
@@ -3,18 +3,12 @@
 
 #include <stdlib.h>
 
-/**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- */
 typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
 } listint_t;
 
-/* function prototype */
 int sum_listint(listint_t *head);
 
 #endif
