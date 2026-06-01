faf2623e1c95065898ffed2a77e8978af9ded6b3
Nvawo Alamar Nvawo <nyawoalamar5@gmail.com>
2026-06-01 15:21:32 +0300

    Fix missing prototype for dlistint_len

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -16,7 +16,8 @@ typedef struct dlistint_s
     struct dlistint_s *next;
 } dlistint_t;
 
-/* function prototype */
+/* function prototypes */
 size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
 
 #endif
