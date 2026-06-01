f3767cdb6ca44fd06d5b9d37663d169a780aecdb
Nvawo Alamar Nvawo <nyawoalamar5@gmail.com>
2026-06-01 12:19:19 +0300

    Task 0: sum of linked list

--- 8-sum_listint.c
+++ 8-sum_listint.c
@@ -1,21 +1,20 @@
 #include "lists.h"
 
 /**
- * sum_listint - returns the sum of all data (n) in a listint_t linked list
- * @head: pointer to the head of the list
+ * sum_listint - returns the sum of all data (n) in a listint_t list
+ * @head: pointer to head of list
  *
- * Return: sum of all n values, or 0 if list is empty
+ * Return: sum of all n, or 0 if list is empty
  */
 int sum_listint(listint_t *head)
 {
-    int sum = 0;
-    listint_t *current = head;
+	int sum = 0;
 
-    while (current != NULL)
-    {
-        sum += current->n;
-        current = current->next;
-    }
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
 
-    return (sum);
+	return (sum);
 }
