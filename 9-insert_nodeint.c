62bda1e4c7bdd7c4e31924e36798cfb6cbc6c55f
Nvawo Alamar <nyawoalamar5@gmail.com>
2026-06-05 10:03:54 +0300

    Fix insert_nodeint_at_index logic

--- 9-insert_nodeint.c
+++ 9-insert_nodeint.c
@@ -5,14 +5,13 @@
  * insert_nodeint_at_index - inserts a new node at a given index
  * @head: pointer to head of list
  * @idx: index to insert node
- * @n: value of new node
+ * @n: value to store in new node
  *
- * Return: pointer to new node, or NULL if it fails
+ * Return: address of new node, or NULL if it fails
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node;
-	listint_t *temp;
+	listint_t *new_node, *temp;
 	unsigned int i;
 
 	if (head == NULL)
