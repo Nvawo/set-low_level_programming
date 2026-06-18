d29d76d8bef595093996607f1a521e7c20d39d87
Nvawo Alamar <nyawoalamar5@gmail.com>
2026-06-18 13:51:48 +0300

    Task 0: hash table creation completed

---
+++ hash_tables/README.md
@@ -0,0 +1,20 @@
+# Hash Tables Project
+
+This project implements a hash table in C as part of the ALX Low Level Programming curriculum.
+
+## Description
+
+A hash table is a data structure that stores key-value pairs and allows fast data retrieval using a hash function.
+
+## Files
+
+- hash_tables.h → Header file containing structures and prototypes
+- 0-hash_table_create.c → Function to create a hash table
+- 0-main.c → Test file for the implementation
+
+## Function
+
+### hash_table_create
+
+```c
+hash_table_t *hash_table_create(unsigned long int size);
---
+++ hash_tables/hash_tables.h
@@ -0,0 +1,33 @@
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
+
+#include <stdlib.h>
+
+/**
+ * struct hash_table_s - hash table structure
+ * @size: size of the array
+ * @array: array of pointers
+ */
+typedef struct hash_table_s
+{
+    unsigned long int size;
+    struct hash_node_s **array;
+} hash_table_t;
+
+/**
+ * struct hash_node_s - node of a hash table
+ * @key: key string
+ * @value: value string
+ * @next: pointer to next node
+ */
+typedef struct hash_node_s
+{
+    char *key;
+    char *value;
+    struct hash_node_s *next;
+} hash_node_t;
+
+/* function prototype */
+hash_table_t *hash_table_create(unsigned long int size);
+
+#endif
